998f16bacf60d9205c5ff7c77a9a3aeff7a42d3a
JCastoro <89142710+JCastoro@users.noreply.github.com>
2021-08-18 13:51:36 -0400

    Checkbook Balancing Exercise

---
+++ Jcc10717_hw12.cpp
@@ -0,0 +1,80 @@
+#include <iostream>
+#include <cstdlib>
+#include <cctype>
+using namespace std;
+
+class Money
+{
+public:
+    friend Money operator +(const Money& amount1, const Money& amount2);
+//Returns the sum of the values of amount1 and amount2.
+
+    friend Money operator -(const Money& amount1, const Money& amount2);
+//Returns amount 1 minus amount2.
+
+    friend Money operator -(const Money& amount);
+//Returns the negative of the value of amount.
+
+    friend bool operator ==(const Money& amount1, const Money& amount2);
+//Returns true if amount1 and amount2 have the same value; false otherwise.
+
+    friend bool operator <(const Money& amount1, const Money& amount2);
+    //Returns true if amount1 is less than amount2; false otherwise.
+
+    Money(long dollars, int cents);
+//Initializes the object so its value represents an amount with
+//the dollars and cents given by the arguments. If the amount
+//is negative, then both dollars and cents should be negative.
+    Money(long dollars);
+//Initializes the object so its value represents $dollars.00.
+    Money();
+   //Initializes the object so its value represents $0.00.
+    double get_value() const;
+    //Returns the amount of money recorded in the data portion of the calling
+    //object.
+
+    friend istream& operator >>(istream& ins, Money& amount);
+    //Overloads the >> operator so it can be used to input values of type
+    //Money. Notation for inputting negative amounts is as in -$100.00.
+    //Precondition: If ins is a file input stream, then ins has already been
+    //connected to a file.
+
+    friend ostream& operator <<(ostream& outs, const Money& amount);
+    //Overloads the << operator so it can be used to output values of type
+    //Money. Precedes each output value of type Money with a dollar sign.
+    //Precondition: If outs is a file output stream, then outs has already been
+    //connected to a file.
+
+private:
+    long all_cents;
+};
+
+int main()
+{
+
+    return 0;
+}
+
+
+//overloaded operators
+
+Money operator +(const Money& amount1, const Money& amount2) {
+
+    Money temp;
+    temp.all_cents = amount1.all_cents + amount2.all_cents;
+    return temp;
+}
+bool operator ==(const Money& amount1, const Money& amount2) {
+    return (amount1.all_cents == amount2.all_cents);
+}
+
+Money operator -(const Money& amount) {
+    Money temp;
+    temp.all_cents = -amount.all_cents;
+    return temp;
+}
+Money operator -(const Money& amount1, const Money& amount2) {
+    Money temp;
+    temp.all_cents = amount1.all_cents - amount2.all_cents;
+    return temp;
+}
